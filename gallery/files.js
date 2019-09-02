ab50a7b747186a1cfbf887050e020b9eeee6b4ac
Grzegorz Ligas <grzegorz.ligas@codnos.com>
2019-09-02 14:38:36 +0100

    Improve the gallery

--- gallery/files.js
+++ gallery/files.js
@@ -1,34 +1,159 @@
 var files = [
-{
-	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	"title": "Section 1",
-	"images": [
-		{"href":'https://dummyimage.com/800/ffffff/000000'},
-		{"href":'https://dummyimage.com/1600/ffffff/000000'}
-	]
-},
-{
-	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	"title": "Section 2",
-	"images": [
-		{"href":'https://dummyimage.com/1280/000000/fffff'},
-		{"href":'https://dummyimage.com/400/000000/ffffff'},
-		{"href":'https://dummyimage.com/800/000000/ffffff'}
-	]
-},
-{
-	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	"title": "Section 3",
-	"images": [
-		{"href":'https://dummyimage.com/1600/000000/ffffff'}
-	]
-},
-{
-	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	"title": "Section 4",
-	"images": [
-		{"href":'https://dummyimage.com/800/ffffff/000000'},
-		{"href":'https://dummyimage.com/1600/ffffff/000000'}
-	]
-},
+  {
+    "title": "19 Oct 2046 10:43",
+    "description": {
+      "Observation": {
+        "date": "19 Oct 2046 10:43",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "eyfs": [
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          }
+        ]
+      },
+      "Next steps": {
+        "date": "20 Oct 2046 10:24",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "eyfs": [
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          }
+        ]
+      }
+    },
+    "images": [
+      {
+        "href": "https://dummyimage.com/800/ffffff/000000",
+        "title": "Observed on 19/10/2046 at 10:41"
+      },
+      {
+        "href": "https://dummyimage.com/1600/ffffff/000000",
+        "title": "Observed on 19/10/2046 at 10:41"
+      }
+    ]
+  },
+  {
+    "title": "19 Oct 2046 16:24",
+    "description": {
+      "Observation": {
+        "date": "19 Oct 2046 16:24",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "eyfs": [
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          }
+        ]
+      },
+      "Next steps": {
+        "date": "20 Oct 2046 10:40",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "eyfs": [
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          }
+        ]
+      }
+    },
+    "images": [
+      {
+        "href": "https://dummyimage.com/1280/000000/fffff",
+        "title": "Observed on 19/10/2046 at 14:05"
+      },
+      {
+        "href": "https://dummyimage.com/400/000000/ffffff",
+        "title": "Observed on 19/10/2046 at 14:06"
+      }
+    ]
+  },
+  {
+    "title": "19 Oct 2046 17:44",
+    "description": {
+      "Observation": {
+        "date": "19 Oct 2046 17:44",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "eyfs": [
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          }
+        ]
+      }
+    },
+    "images": [
+      {
+        "href": "https://dummyimage.com/1600/000000/ffffff",
+        "title": "Observed on 19/10/2046 at 17:35"
+      }
+    ]
+  },
+  {
+    "title": "30 Oct 2046 10:43",
+    "description": {
+      "Observation": {
+        "date": "30 Oct 2046 10:43",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "eyfs": [
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          },
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          }
+        ]
+      },
+      "Next steps": {
+        "date": "30 Oct 2046 10:24",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "eyfs": [
+          {
+            "Lorem - Ipsum": "\"dolor sit amet, consectetur adipiscing elit\""
+          }
+        ]
+      }
+    }
+  }
 ];
